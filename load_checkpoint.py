70af606ead6b918245ab0a1521d27b60a367eb37
NicolasAraya932 <63214589+NicolasAraya932@users.noreply.github.com>
2025-07-27 18:47:38 -0400

    roi framework first approaches

---
+++ load_checkpoint.py
@@ -0,0 +1,123 @@
+import argparse
+from dataclasses import dataclass, field
+import torch
+from torch.cuda.amp.grad_scaler import GradScaler
+from collections import defaultdict
+from threading import Lock
+from typing import TYPE_CHECKING
+
+
+from pathlib import Path
+from nerfstudio.utils.rich_utils import CONSOLE
+from nerfstudio.engine.trainer import Trainer
+from nerfstudio.configs.experiment_config import ExperimentConfig
+from typing import DefaultDict, Dict, List, Literal, Optional, Tuple, Type, cast
+
+
+from nerfstudio.pipelines.base_pipeline import VanillaPipeline
+from nerfstudio.engine.optimizers import Optimizers
+from nerfstudio.engine.callbacks import TrainingCallback, TrainingCallbackAttributes, TrainingCallbackLocation
+
+TRAIN_INTERATION_OUTPUT = Tuple[torch.Tensor, Dict[str, torch.Tensor], Dict[str, torch.Tensor]]
+TORCH_DEVICE = str
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--checkpoint", type=str, default="checkpoint.pth", help="Path to the checkpoint file")
+args = parser.parse_args()
+
+checkpoint = torch.load(args.checkpoint, map_location='cpu')
+
+@dataclass
+class TrainerConfig(ExperimentConfig):
+    """Configuration for training regimen"""
+
+    _target: Type = field(default_factory=lambda: Trainer)
+    """target class to instantiate"""
+    steps_per_save: int = 1000
+    """Number of steps between saves."""
+    steps_per_eval_batch: int = 500
+    """Number of steps between randomly sampled batches of rays."""
+    steps_per_eval_image: int = 500
+    """Number of steps between single eval images."""
+    steps_per_eval_all_images: int = 25000
+    """Number of steps between eval all images."""
+    max_num_iterations: int = 1000000
+    """Maximum number of iterations to run."""
+    mixed_precision: bool = False
+    """Whether or not to use mixed precision for training."""
+    use_grad_scaler: bool = False
+    """Use gradient scaler even if the automatic mixed precision is disabled."""
+    save_only_latest_checkpoint: bool = True
+    """Whether to only save the latest checkpoint or all checkpoints."""
+    # optional parameters if we want to resume training
+    load_dir: Optional[Path] = None
+    """Optionally specify a pre-trained model directory to load from."""
+    load_step: Optional[int] = None
+    """Optionally specify model step to load from; if none, will find most recent model in load_dir."""
+    load_config: Optional[Path] = None
+    """Path to config YAML file."""
+    load_checkpoint: Optional[Path] = None
+    """Path to checkpoint file."""
+    log_gradients: bool = False
+    """Optionally log gradients during training"""
+    gradient_accumulation_steps: Dict[str, int] = field(default_factory=lambda: {})
+    """Number of steps to accumulate gradients over. Contains a mapping of {param_group:num}"""
+    start_paused: bool = False
+    """Whether to start the training in a paused state."""
+
+
+class Trainer:
+    """Trainer class
+
+    Args:
+        config: The configuration object.
+        local_rank: Local rank of the process.
+        world_size: World size of the process.
+
+    Attributes:
+        config: The configuration object.
+        local_rank: Local rank of the process.
+        world_size: World size of the process.
+        device: The device to run the training on.
+        pipeline: The pipeline object.
+        optimizers: The optimizers object.
+        callbacks: The callbacks object.
+        training_state: Current model training state.
+    """
+
+    pipeline: VanillaPipeline
+    optimizers: Optimizers
+    callbacks: List[TrainingCallback]
+
+    def __init__(self, config: TrainerConfig, local_rank: int = 0, world_size: int = 1) -> None:
+        self.train_lock = Lock()
+        self.config = config
+        self.local_rank = local_rank
+        self.world_size = world_size
+        self.device: TORCH_DEVICE = config.machine.device_type
+        if self.device == "cuda":
+            self.device += f":{local_rank}"
+        self.mixed_precision: bool = self.config.mixed_precision
+        self.use_grad_scaler: bool = self.mixed_precision or self.config.use_grad_scaler
+        self.training_state: Literal["training", "paused", "completed"] = (
+            "paused" if self.config.start_paused else "training"
+        )
+        self.gradient_accumulation_steps: DefaultDict = defaultdict(lambda: 1)
+        self.gradient_accumulation_steps.update(self.config.gradient_accumulation_steps)
+
+        if self.device == "cpu":
+            self.mixed_precision = False
+            CONSOLE.print("Mixed precision is disabled for CPU training.")
+        self._start_step: int = 0
+        # optimizers
+        self.grad_scaler = GradScaler(enabled=self.use_grad_scaler)
+
+        self.base_dir: Path = config.get_base_dir()
+        # directory to save checkpoints
+        self.checkpoint_dir: Path = config.get_checkpoint_dir()
+        CONSOLE.log(f"Saving checkpoints to: {self.checkpoint_dir}")
+
+        self.viewer_state = None
+
+        # used to keep track of the current step
+        self.step = 0
